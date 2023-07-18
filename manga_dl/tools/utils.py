18c9f5f57aa2a15969d8ab677ec2714746cef9f2
Sifat <hossain0338@gmail.com>
2023-07-19 00:16:29 +0600

    BUG fix

--- manga_dl/tools/utils.py
+++ manga_dl/tools/utils.py
@@ -199,6 +199,7 @@ class DriverManager:
     def driver_options(self):
         options = webdriver.ChromeOptions()
         options.add_argument("--headless")
+        options.add_argument('--window-size=1420,1080')
         options.add_argument("--blink-settings=imagesEnabled=false")
         options.add_argument("--no-sandbox")
         options.add_argument("--disable-dev-shm-usage")
